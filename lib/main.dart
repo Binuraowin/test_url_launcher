6d69e8b0f1277cc8750470bd78721e0f7b38bb26
Binura owin <binura.owin1@gmail.com>
2020-05-31 15:00:15 +0530

    geolacotor not works

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
-
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
